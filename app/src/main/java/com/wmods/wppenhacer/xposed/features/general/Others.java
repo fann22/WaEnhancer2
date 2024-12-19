b54fb6684850566fd8b0043902c189b356c6091b
irfan <116457057+fann22@users.noreply.github.com>
2024-12-19 09:28:21 +0700

    Update Others.java

--- app/src/main/java/com/wmods/wppenhacer/xposed/features/general/Others.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/features/general/Others.java
@@ -7,6 +7,7 @@ import android.text.TextUtils;
 import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
+import android.content.Intent;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
