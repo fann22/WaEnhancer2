aee390249fc428d868f4daa09585cd8ac55474e9
irfan <116457057+fann22@users.noreply.github.com>
2024-12-20 18:19:37 +0700

    Update Unobfuscator.java

--- app/src/main/java/com/wmods/wppenhacer/xposed/core/devkit/Unobfuscator.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/core/devkit/Unobfuscator.java
@@ -1780,7 +1780,7 @@ public class Unobfuscator {
             if (methodData.isEmpty()) throw new RuntimeException("ConversationListView method not found");
             for (var data : methodData) {
                 //data.setAccessible(true);
-                XposedBridge.log(data.toString());
+                XposedBridge.log(data.getDescriptor().toString());
             }
             return methodData.get(0).getMethodInstance(loader);
         });
