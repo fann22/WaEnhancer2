114774199bb3b8ea2458fb655042d1e907b1d110
irfan <116457057+fann22@users.noreply.github.com>
2024-12-20 09:29:12 +0700

    Update Unobfuscator.java

--- app/src/main/java/com/wmods/wppenhacer/xposed/core/devkit/Unobfuscator.java
+++ app/src/main/java/com/wmods/wppenhacer/xposed/core/devkit/Unobfuscator.java
@@ -1771,6 +1771,7 @@ public class Unobfuscator {
             var methodData = dexkit.findMethod(FindMethod.create().matcher(
                     MethodMatcher.create().addUsingString("Callback must be disconnected before connecting a different callback")
             ));
+            XposedBrige.log(methodData.toString());
             if (methodData.isEmpty()) throw new RuntimeException("ConversationListView method not found");
             return methodData.get(0).getMethodInstance(loader);
         });
